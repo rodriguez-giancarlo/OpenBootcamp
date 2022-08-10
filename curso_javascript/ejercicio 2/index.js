c5332d31abb247e1e1c34e8dd2c286e655101082
rodriguez-giancarlo <92653419+rodriguez-giancarlo@users.noreply.github.com>
2022-08-10 18:59:20 -0500

    ejercicio 2

---
+++ curso_javascript/ejercicio 2/index.js
@@ -0,0 +1,24 @@
+//Tu nombre (string)
+let nombre = "giancarlo"
+console.log(nombre)
+// Tu edad (number)
+let edad = 25
+console.log(edad)
+
+// ¿Eres desarrollador? (boolean)
+let isDesarrollador = true
+console.log(isDesarrollador)
+
+//Tu fecha de nacimiento (Date)
+const FechaNacimiento = new Date("1997-02-27")
+console.log(FechaNacimiento)
+
+//Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+const LibroFavorito={
+    "titulo": "100 años de soledad",
+    "autor": "Gabriel García Márquez",
+    "fecha":"1967",
+    "url":"https://www.amazon.com/-/es/Gabriel-Garc%C3%ADa-M%C3%A1rquez/dp/0307474720"
+}
+
+console.log(LibroFavorito.titulo)
